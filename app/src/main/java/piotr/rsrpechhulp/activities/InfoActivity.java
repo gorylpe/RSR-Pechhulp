3463184164b108bfe6dfdcb3ed906a59d9e64239
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2018-05-04 00:01:29 +0200

    info activity and some refactoring

--- app/src/main/java/piotr/rsrpechhulp/activities/InfoActivity.java
+++ app/src/main/java/piotr/rsrpechhulp/activities/InfoActivity.java
@@ -2,6 +2,7 @@ package piotr.rsrpechhulp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import piotr.rsrpechhulp.R;
 
 public class InfoActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+
+    public void buttonBack(View view) {
+        finish();
     }
 }
--- app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
+++ app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package piotr.rsrpechhulp.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import piotr.rsrpechhulp.R;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+
+    public void buttonInfoClick(View view) {
+        startActivity(new Intent(this, InfoActivity.class));
     }
 }
