21bf785b2d834b3d583f3f93c3c610c842508886
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2018-05-03 21:23:31 +0200

    main menu design

--- app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
+++ app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
@@ -1,9 +1,7 @@
 package piotr.rsrpechhulp.activities;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import piotr.rsrpechhulp.R;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -12,21 +10,5 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-    }
-
-    private void setupActionBar() {
-        // Inflate your custom layout
-        final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
-                R.layout.custom_actionbar,
-                null);
-
-        // Set up your ActionBar
-        final ActionBar actionBar = getSupportActionBar();
-        if(null == actionBar)
-            return;
-        actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setCustomView(actionBarLayout);
     }
 }
