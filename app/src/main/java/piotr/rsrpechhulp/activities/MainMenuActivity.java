e61fea3a7b4978284402da836c02e7fd4e5f6a10
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2018-05-03 22:34:31 +0200

    relativelayout as toolbar

--- app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
+++ app/src/main/java/piotr/rsrpechhulp/activities/MainMenuActivity.java
@@ -2,7 +2,6 @@ package piotr.rsrpechhulp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import piotr.rsrpechhulp.R;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -11,8 +10,5 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
-        Toolbar myToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
     }
 }
