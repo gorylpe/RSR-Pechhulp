bc2d85d23a1e1b94e0b898eb7d9d8c312f6ac852
Piotr Przestrzelski <p.przestrzelski@protonmail.com>
2018-05-03 22:23:03 +0200

    added info activity and toolbar changes

--- app/src/main/java/piotr/rsrpechhulp/activities/InfoActivity.java
+++ app/src/main/java/piotr/rsrpechhulp/activities/InfoActivity.java
@@ -2,17 +2,13 @@ package piotr.rsrpechhulp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import piotr.rsrpechhulp.R;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-        Toolbar myToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+        setContentView(R.layout.activity_info);
     }
 }
